12c1526e6b4a6d5ef2f9080241ea86cfdefba05a
yesir-sys-s <warr1432@gmail.com>
2025-04-18 12:15:40 +0800

    commit

--- albums/admin.py
+++ albums/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+from .models import Album, Photo
 
-# Register your models here.
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('created_at', 'created_by')
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('album', 'caption', 'uploaded_at', 'image_preview')
+    search_fields = ('caption', 'album__title')
+    list_filter = ('uploaded_at', 'album')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="150" />')
+        return "No image"
+    image_preview.short_description = 'Preview'
